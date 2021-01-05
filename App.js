2132e1f9ebf893d196946d76bc76269147805aeb
apipshato <afifshatos@gmail.com>
2021-01-05 12:41:22 +0700

    fix notificiation position

--- App.js
+++ App.js
@@ -1,80 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {  ScrollView} from 'react-native';
-import styled from 'styled-components';
-import Card from './components/Card';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { ScrollView, SafeAreaView } from "react-native";
+import styled from "styled-components";
+import Card from "./components/Card";
+import * as Icon from '@expo/vector-icons'
+
 
 export default function App() {
   return (
-    <Container >
-    <ScrollView style={{height: "100%"}}>
-    <TitleBar>
-    <Avatar source={require('./assets/avatar.jpg')}/>
-      <Title>Welcome Back, </Title>
-      <Name>Apep</Name>
-      <StatusBar style="auto" />
-      </TitleBar>
-      <Subtitle>
-        Continue Learning
-      </Subtitle>
-      <ScrollView horizontal={true} style={{paddingBottom: 30}}>
-      <Card title="Styled Components"
-       image={require('./assets/background2.jpg')}
-       caption="React Native"
-       logo={require('./assets/logo-react.png')}
-       subtitle="5 0f 12 sections"
-       />
-        <Card title="Styled Components 2"
-       image={require('./assets/background1.jpg')}
-       caption="React Native"
-       logo={require('./assets/logo-react.png')}
-       subtitle="5 0f 12 sections"
-       />
-       </ScrollView>
-       </ScrollView>
+    <Container>
+      <SafeAreaView>
+        <ScrollView style={{ height: "100%" }}  contentContainerStyle={{ flexGrow: 1 }}>
+          <TitleBar>
+            <Avatar source={require("./assets/avatar.jpg")} />
+            <Title>Welcome Back, </Title>
+            <Name>Apep</Name>
+            <Icon.Ionicons name="notifications" 
+            size={32} 
+            color="#4475f2"
+            style={{position:"absolute", right: 20}}
+            />
+            <StatusBar style="auto" />
+          </TitleBar>
+          <Subtitle>Continue Learning</Subtitle>
+          <ScrollView
+            horizontal={true}
+            style={{ paddingBottom: 30 }}
+            showsHorizontalScrollIndicator={false}
+           
+          >
+            <Card
+              title="Styled Components"
+              image={require("./assets/background2.jpg")}
+              caption="React Native"
+              logo={require("./assets/logo-react.png")}
+              subtitle="5 0f 12 sections"
+            />
+            <Card
+              title="Styled Components 2"
+              image={require("./assets/background1.jpg")}
+              caption="React Native"
+              logo={require("./assets/logo-react.png")}
+              subtitle="5 0f 12 sections"
+            />
+          </ScrollView>
+        </ScrollView>
+      </SafeAreaView>
     </Container>
   );
 }
 
-const Subtitle =styled.Text`
-color: #b8bece;
-font-weight: 600;
-font-size: 15px;
-margin-left: 20px;
-margin-top: 50px;
-text-transform: uppercase;
-`
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
 
-const Avatar =styled.Image`
-width: 44px;
-height: 44px;
-background: black;
-border-radius: 22px;
-margin-left: 20px;
-position: absolute;
-top: 0;
-left: 0;
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
 
-`
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+`;
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
 
-const Container=styled.View`
-flex : 1;
-background-color: #f0f3f5;
-`
-const Title=styled.Text`
-font-size: 16px;
-color: #b8bece;
-font-weight: 500;
-
-`
-const Name=styled.Text`
-font-size: 20px;
-color: #3c4560;
-font-weight: bold;
-`
-
-const TitleBar =styled.View`
-width: 100%;
-margin-top: 50px;
-padding-left:80px;
-`
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`;
