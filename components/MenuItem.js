7850d926188b629b6ae463ac030d5a1701d5b92a
apipshato <afifshatos@gmail.com>
2021-01-14 13:42:21 +0700

    Update MenuItem.js

--- components/MenuItem.js
+++ components/MenuItem.js
@@ -4,6 +4,13 @@ import * as Icon from '@expo/vector-icons'
 
 const MenuItem= props => (
     <Container>
-        
+        <IconView>
+            <Icon.Ionicons name={props.icon} size={24} color="#546bfb"/>
+        </IconView>
+        <Content>
+            <Title>{props.title}</Title>
+            <Text>{props.text}</Text>
+
+        </Content>
     </Container>
 )
