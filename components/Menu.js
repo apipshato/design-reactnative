e03ed331c51ba8dddadc3c48268796b4bf9d3d24
apipshato <afifshatos@gmail.com>
2021-01-14 15:50:48 +0700

    add menu item fix

--- components/Menu.js
+++ components/Menu.js
@@ -1,125 +1,133 @@
-import React from 'react'
-import styled from "styled-components"
-import { Animated, LogBox, TouchableOpacity, Dimensions } from 'react-native'
-import * as Icon from '@expo/vector-icons'
-import MenuItem from './MenuItem'
+import React from "react";
+import styled from "styled-components";
+import { Animated, LogBox, TouchableOpacity, Dimensions } from "react-native";
+import * as Icon from "@expo/vector-icons";
+import MenuItem from "./MenuItem";
 
-
-const screenHeight= Dimensions.get("window").height 
+const screenHeight = Dimensions.get("window").height;
 
 class Menu extends React.Component {
-state={
-    top: new Animated.Value(screenHeight)
-};
-componentDidMount(){
-    LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
+  state = {
+    top: new Animated.Value(screenHeight),
+  };
+  componentDidMount() {
+    LogBox.ignoreLogs(["Animated: `useNativeDriver`"]);
 
     Animated.spring(this.state.top, {
-        toValue: 0,
-        
-    }).start()
-}
+      toValue: 0,
+    }).start();
+  }
 
-toggleMenu =()=>{
+  toggleMenu = () => {
     Animated.spring(this.state.top, {
-        toValue: screenHeight
-    }).start()
-}
-    render(){
-        return(
-            <AnimatedContainer style={{ top: this.state.top }}>
-                <Cover>
-                    <Image source={require("../assets/background2.jpg")}/>
-                    <Title>Afif Shato</Title>
-                    <Subtitle>Designer for Design + Code</Subtitle>
-                </Cover>
-                <TouchableOpacity onPress={this.toggleMenu} style={{position: "absolute", top: 120,
-                 left: "50%",marginLeft: -22, zIndex: 1 }}>
-                <CloseView>
-                    <Icon.Ionicons name="ios-close" size={44} color="#546bfb" />
-                </CloseView>
-                </TouchableOpacity>
-                <Content>
-                    <MenuItem/>
-                </Content>
-            </AnimatedContainer>
-        )
-    }
+      toValue: screenHeight,
+    }).start();
+  };
+  render() {
+    return (
+      <AnimatedContainer style={{ top: this.state.top }}>
+        <Cover>
+          <Image source={require("../assets/background2.jpg")} />
+          <Title>Afif Shato</Title>
+          <Subtitle>Designer for Design + Code</Subtitle>
+        </Cover>
+        <TouchableOpacity
+          onPress={this.toggleMenu}
+          style={{
+            position: "absolute",
+            top: 120,
+            left: "50%",
+            marginLeft: -22,
+            zIndex: 1,
+          }}
+        >
+          <CloseView>
+            <Icon.Ionicons name="ios-close" size={44} color="#546bfb" />
+          </CloseView>
+        </TouchableOpacity>
+        <Content>
+          {items.map((item, index) => (
+            <MenuItem
+              key={index}
+              icon={item.icon}
+              title={item.title}
+              text={item.text}
+            />
+          ))}
+        </Content>
+      </AnimatedContainer>
+    );
+  }
 }
 export default Menu;
 
-const Image =styled.Image`
-position: absolute;
-width:100%;
-height:100%;
+const Image = styled.Image`
+  position: absolute;
+  width: 100%;
+  height: 100%;
 `;
-const Title= styled.Text`
-color: white;
-font-size: 24px;
-font-weight: 600;
+const Title = styled.Text`
+  color: white;
+  font-size: 24px;
+  font-weight: 600;
 `;
-const Subtitle= styled.Text`
-font-size: 13px;
-color: rgba(255,255,255, 0.5);
-margin-top: 8px;
+const Subtitle = styled.Text`
+  font-size: 13px;
+  color: rgba(255, 255, 255, 0.5);
+  margin-top: 8px;
 `;
-const CloseView=styled.View`
-width:44px;
-height:44px;
-border-radius: 22px;
-background: white;
-justify-content: center;
-align-items: center;
-box-shadow: 0 5px 10px rgba(0,0,0, 0.15)
-`;
-
-
-const Container =styled.View`
-position: absolute;
-background: white;
-width: 100%;
-height: 100%;
-z-index: 100;
+const CloseView = styled.View`
+  width: 44px;
+  height: 44px;
+  border-radius: 22px;
+  background: white;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
 `;
 
-
-const AnimatedContainer= Animated.createAnimatedComponent(Container);
-
-const Cover =styled.View`
-height: 142px;
-background: black;
-justify-content: center;
-align-items: center;
+const Container = styled.View`
+  position: absolute;
+  background: white;
+  width: 100%;
+  height: 100%;
+  z-index: 100;
 `;
 
-const Content =styled.View`
-height: ${screenHeight};
-background: #f0f3f5;
+const AnimatedContainer = Animated.createAnimatedComponent(Container);
+
+const Cover = styled.View`
+  height: 142px;
+  background: black;
+  justify-content: center;
+  align-items: center;
 `;
 
-const items=[
-    {
-        icon: "ios-settings",
-        title: "Account",
-        text: "settings",
+const Content = styled.View`
+  height: ${screenHeight};
+  background: #f0f3f5;
+  padding: 50px;
+`;
 
-    },
-    {
-        icon: "ios-card",
-        title: "Billing",
-        text: "payments",
-        
-    },
-    {
-        icon: "ios-compass",
-        title: "Learn React",
-        text: "start course",
-        
-    },
-    {
-        icon: "ios-exit",
-        title: "Log Out",
-        text: "see you soon!",
-        
-    }
-]
+const items = [
+  {
+    icon: "ios-settings",
+    title: "Account",
+    text: "settings",
+  },
+  {
+    icon: "ios-card",
+    title: "Billing",
+    text: "payments",
+  },
+  {
+    icon: "ios-compass",
+    title: "Learn React",
+    text: "start course",
+  },
+  {
+    icon: "ios-exit",
+    title: "Log Out",
+    text: "see you soon!",
+  },
+];
--- components/MenuItem.js
+++ components/MenuItem.js
@@ -17,8 +17,28 @@ const MenuItem= props => (
 
 export default MenuItem;
 
- const Container= styled.View``;
- const IconView= styled.View``;
- const Content= styled.View``;
- const Title= styled.Text``;
- const Text= styled.Text``;
+ const Container= styled.View`
+ flex-direction: row;
+ margin: 15px 0;
+ `;
+ const IconView= styled.View`
+ width: 32px;
+ height: 32px;
+ justify-content: center;
+ align-items: center;
+ `;
+ const Content= styled.View`
+ padding-left: 20px;
+ `;
+
+ const Title= styled.Text`
+ color: #3c4560;
+ font-size: 24px;
+ font-weight:bold;
+ `;
+ const Text= styled.Text`
+color: #3c4560;
+font-weight: 600;
+opacity:0.6;
+margin-top: 5px;
+ `;
